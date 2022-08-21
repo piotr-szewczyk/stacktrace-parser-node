ce17120642df4c0937ecac7ca23512659cb3a6c8
Piotrek Szewczyk <p.szewczyk@doittechnology.pl>
2022-08-21 15:32:41 +0200

    initial commit

---
+++ src/types.ts
@@ -0,0 +1,24 @@
+export interface Trace {
+    filename?: string;
+    function?: string;
+    lineNo?: number;
+    columnNo?: number;
+    internal?: boolean;
+    absPath?: string;
+    extension?: string;
+    preCode?: string[];
+    code?: string;
+    postCode?: string[];
+}
+
+export interface SourceCode {
+    code: string;
+    preCode: string[];
+    postCode: string[];
+}
+
+export interface ErrorResponse {
+    message: string;
+    name: string;
+    traces: Trace[];
+}
